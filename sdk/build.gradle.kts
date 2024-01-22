4516ea78280896fa1d8b7b7adf7acccdcc5c629e
Tornaco <Tornaco@163.com>
2024-01-22 17:52:04 +0800

    add mvn publish

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.protobuf.gradle.plugin)
+    id("maven-publish")
 }
 
 dependencies {
@@ -17,5 +18,31 @@ protobuf {
     protoc {
         // The artifact spec for the Protobuf Compiler
         artifact = "com.google.protobuf:protoc:3.25.2"
+    }
+}
+
+
+val GROUP_ID = "shortx"
+val ARTIFACT_ID = "plugin.sdk"
+val VERSION = latestGitTag().ifEmpty { "1.0.0" }
+
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return process.inputStream.bufferedReader().use { bufferedReader ->
+        bufferedReader.readText().trim()
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = GROUP_ID
+            artifactId = ARTIFACT_ID
+            version = VERSION
+
+            afterEvaluate {
+                from(components["java"])
+            }
+        }
     }
 }
