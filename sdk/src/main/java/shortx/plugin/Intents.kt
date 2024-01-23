fb1fefe3f2d3beccdeb68e5be838b3df4c3c0700
Tornaco <Tornaco@163.com>
2024-01-23 09:18:23 +0800

    add more const

--- sdk/src/main/java/shortx/plugin/Intents.kt
+++ sdk/src/main/java/shortx/plugin/Intents.kt
@@ -2,4 +2,9 @@ package shortx.plugin
 
 object Intents {
     const val EXTRA_ACTION_PICKER_RESULT = "shortx.extra.action.picker.result"
+
+    const val META_DATA_ACTION_EVALUATOR_ENTRY = "shortx.plugin.action.evaluator.entry"
+    const val META_DATA_PLUGIN_DESCRIPTION = "shortx.plugin.description"
+    const val META_DATA_PLUGIN_NAME = "shortx.plugin.name"
+    const val META_DATA_PLUGIN = "shortx.plugin"
 }
