694e9580c413dcededa9d16ff0d2d04e091b7bd9
Tornaco <Tornaco@163.com>
2024-01-23 16:13:18 +0800

    remove suspend funs

--- sdk/src/main/java/shortx/plugin/ActionEvaluator.kt
+++ sdk/src/main/java/shortx/plugin/ActionEvaluator.kt
@@ -3,13 +3,11 @@ package shortx.plugin
 import android.content.Context
 import androidx.annotation.Keep
 
-interface ActionEvaluator {
-    data class Input(
-        val androidContext: Context,
-        val contextData: Map<String, String>,
-        val paramsData: ParamsData
-    )
+@Keep
+abstract class ActionEvaluator {
+    fun invokeEvaluate(androidContext: Context, paramsData: ByteArray) {
+        evaluate(androidContext, ParamsData.parseFrom(paramsData))
+    }
 
-    @Keep
-    suspend fun evaluate(input: Input)
+    abstract fun evaluate(androidContext: Context, paramsData: ParamsData)
 }
